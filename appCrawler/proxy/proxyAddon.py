bd4330f85ad336fd23a820d86d46399a4775f65c
yqx254 <ravix254@outlook.com>
2021-04-13 17:28:10 +0800

    弹幕解析成功，开始进一步分析其他数据结构

--- appCrawler/proxy/proxyAddon.py
+++ appCrawler/proxy/proxyAddon.py
@@ -1,15 +1,31 @@
 import mitmproxy.http
-from mitmproxy import ctx
+import re
+import msg_pb2
+import payload_pb2
 
 
 class Dealer:
     def __init__(self):
         self.num = 0
 
-    def request(self):
-        self.num += 1
-        ctx.log.info("%s request(s) catched! " % self.num)
+    # def request(self, flow: mitmproxy.http.HTTPFlow):
+    #     if flow.request.host == "webcast.amemv.com" \
+    #             and re.match("^/webcast/im/fetch.*$", flow.request.path):
+    #         print("catch!")
+    #         print(flow.response)
 
+    def response(self, flow: mitmproxy.http.HTTPFlow):
+        if flow.request.host == "webcast.amemv.com" \
+                and re.match("^/webcast/im/fetch.*$", flow.request.path):
+            msg = msg_pb2.Response()
+            msg.ParseFromString(flow.response.content)
+            for x in msg.messages:
+                # print(x.method)
+                if x.method == "WebcastChatMessage":
+                    payload = payload_pb2.Detail()
+                    payload.ParseFromString(x.payload)
+                    user = payload.Type()
+                    print(payload.chatMessage)
 
 addons = [
     Dealer()
